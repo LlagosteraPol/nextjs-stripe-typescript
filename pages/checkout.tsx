49cbe3287719a4867fbd4e6641e564958e4ceb6e
Pol92 <plb92@hotmail.com>
2021-04-27 13:39:59 +0200

    Coding example. Error with public_key

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,6 +1,29 @@
+import {loadStripe} from '@stripe/stripe-js'
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) 
 
 export default function Checkout(){
-    return (<div>
-        <h1>Checkout</h1>
-    </div>)
+    const handleClick = async (event) => {
+        // Call your backend to create the Checkout Session
+        const {sessionId} = await fetch('/api/checkout/session', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({quantity: 1})
+        }).then(res => res.json())
+    
+        // When the customer clicks on the button, redirect them to Checout
+        const stripe = await stripePromise;
+
+        const {error} = await stripe.redirectToCheckout({
+            sessionId,
+        });
+    }
+    return (
+        <div>
+            <h1>Checkout</h1>
+            <button role="link" onClick={handleClick}>
+                Checkout
+            </button>
+        </div>)
 }
